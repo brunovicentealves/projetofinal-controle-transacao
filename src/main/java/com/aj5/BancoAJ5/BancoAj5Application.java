86a2fe37a27cd0a29fcb19dffa44f313c166fbc0
Bruno Alves <brunovicentealves.ti@gmail.com>
2020-02-29 12:28:22 -0300

    Projeto Criado

---
+++ src/main/java/com/aj5/BancoAJ5/BancoAj5Application.java
@@ -0,0 +1,14 @@
+package com.aj5.BancoAJ5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoAj5Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BancoAj5Application.class, args);
+	}
+
+}
