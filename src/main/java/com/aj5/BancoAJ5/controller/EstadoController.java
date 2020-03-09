1d777cad22a14e277fabae1302941ded04724c34
Bruno Alves <brunovicentealves.ti@gmail.com>
2020-03-09 17:38:27 -0300

    criado os pacotes repository , service e  controller

---
+++ src/main/java/com/aj5/BancoAJ5/controller/EstadoController.java
@@ -0,0 +1,13 @@
+package com.aj5.BancoAJ5.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/estado")
+public class EstadoController {
+
+
+
+
+}
---
+++ src/main/java/com/aj5/BancoAJ5/service/EstadoService.java
@@ -0,0 +1,12 @@
+package com.aj5.BancoAJ5.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EstadoService {
+
+
+
+
+
+}
