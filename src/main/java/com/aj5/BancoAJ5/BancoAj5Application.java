e49e8b391dce1956e9821b9dbfcd7cdd61bf9e8b
Bruno Alves <brunovicentealves.ti@gmail.com>
2020-03-10 14:05:37 -0300

    implementado o  swagger na API

--- src/main/java/com/aj5/BancoAJ5/BancoAj5Application.java
+++ src/main/java/com/aj5/BancoAJ5/BancoAj5Application.java
@@ -2,13 +2,46 @@ package com.aj5.BancoAJ5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BancoAj5Application {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(BancoAj5Application.class, args);
 	}
+
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.aj5.BancoAJ5.controller")).build().apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		ApiInfo apiInfo = new ApiInfo(
+				"API REST - Controle de transação Bancaria",
+				"Foi criado essa Api para guardar todas as transações bancarias ",
+				"Versão API 1.0",
+				"Termos de uso",
+				"contato@e-mail.com",
+				"API License",
+				"API License URL"
+		);
+		return apiInfo;
+	}
+
+
+
 
 }
--- src/main/java/com/aj5/BancoAJ5/controller/EstadoController.java
+++ src/main/java/com/aj5/BancoAJ5/controller/EstadoController.java
@@ -1,9 +1,25 @@
 package com.aj5.BancoAJ5.controller;
 
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/estado")
 public class EstadoController {
+
+
+    @ApiOperation(value="Consulta todos clientes do sistema", notes="Consulta síncrona.")
+    @GetMapping
+    public ResponseEntity consultaClientes() {
+        return ResponseEntity.status(HttpStatus.OK).body("Consulta de clientes executada com sucesso!");
+    }
+
 }
