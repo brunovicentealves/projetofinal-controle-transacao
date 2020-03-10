297934cef63d47749fbf220881402634cf0160fe
Allan <allan_f2008@hotmail.com>
2020-03-09 23:03:15 -0300

    cria repository das entidades

--- src/main/java/com/aj5/BancoAJ5/repository/EstadoRepository.java
+++ src/main/java/com/aj5/BancoAJ5/repository/EstadoRepository.java
@@ -1,9 +1,10 @@
 package com.aj5.BancoAJ5.repository;
 
 import com.aj5.BancoAJ5.domains.Estado;
-import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EstadoRespository extends CrudRepository<Estado,Long> {
+public interface EstadoRepository extends JpaRepository<Estado,Long> {
 }
