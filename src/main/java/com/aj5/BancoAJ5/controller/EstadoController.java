031ed2efe4f12ede3495027a00548bab0822d4b0
Allan <allan_f2008@hotmail.com>
2020-03-10 21:14:30 -0300

    inicia crud

--- src/main/java/com/aj5/BancoAJ5/controller/EstadoController.java
+++ src/main/java/com/aj5/BancoAJ5/controller/EstadoController.java
@@ -1,9 +1,53 @@
 package com.aj5.BancoAJ5.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import java.util.Optional;
 
-@Controller
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aj5.BancoAJ5.domains.Estado;
+import com.aj5.BancoAJ5.service.EstadoService;
+
+@RestController
 @RequestMapping("/estado")
 public class EstadoController {
+	
+	private EstadoService service;
+
+	public EstadoController(EstadoService service) {
+		this.service = service;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Estado>> getAll(){
+		return ResponseEntity.ok(this.service.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Optional<Estado>> getByID(@PathVariable("id") Long id){
+		return ResponseEntity.ok(this.service.getById(id));
+	}
+	
+	@PostMapping
+	public String save(@RequestBody Estado estado) {
+		return this.service.create(estado).toString();
+	}
+	
+	@DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") Long id) {
+    	this.service.remove(id);
+    	return "Estado " + id.toString() + " deletado.";
+    }
+	
+	
 }
--- src/main/java/com/aj5/BancoAJ5/service/EstadoService.java
+++ src/main/java/com/aj5/BancoAJ5/service/EstadoService.java
@@ -1,12 +1,36 @@
 package com.aj5.BancoAJ5.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+
+import com.aj5.BancoAJ5.domains.Estado;
+import com.aj5.BancoAJ5.repository.EstadoRepository;
 
 @Service
 public class EstadoService {
 
+	private EstadoRepository repository;
 
-
-
-
+	public EstadoService(EstadoRepository repository) {
+		super();
+		this.repository = repository;
+	}
+	
+	public List<Estado> findAll(){
+		return this.repository.findAll();
+	}
+	
+	public Optional<Estado> getById(Long id) {
+		return this.repository.findById(id);
+	}
+	
+	public Estado create(Estado estado) {
+		return this.repository.save(estado);
+	}
+	
+	public void remove(Long id) {
+		this.repository.deleteById(id);
+	}
 }
